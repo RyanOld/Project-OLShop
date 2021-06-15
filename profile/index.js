12a31e73d1d98d28821c5551ce2b899d0ff1a965
Aziz Nurhidayat <aziznurhidayata@gmail.com>
2021-06-15 22:01:50 +0700

    finished my profile. need edit profile and some styling again

--- profile/index.js
+++ profile/index.js
@@ -2,3 +2,55 @@ homeBtn = document.querySelector(".home");
 homeBtn.onclick = () => {
   window.location.href = '../home';
 }
+myProfileBtn = document.querySelector(".myprofile");
+myProfileBtn.onclick = () => {
+  window.location.href = './';
+}
+editBtn = document.querySelector(".edit");
+editBtn.onclick = () => {
+  window.location.href = './edit';
+}
+
+const logoutButton = document.querySelector(".logout");
+logoutButton.addEventListener("click", () => {
+  localStorage['currentuser'] = '';
+  localStorage['jwt'] = '';
+  window.location.href = "../home";
+})
+
+const firstLastName = document.querySelector('#name');
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const cellphone = document.querySelector('#cellphone');
+
+let auth = 'Bearer ' + localStorage['jwt'];
+let currentUser = {};
+
+fetch('http://localhost:1337/users/me', {
+    method: 'GET',
+    headers: {
+      /*
+      'Content-Type': 'application/json',
+      'Connection' : 'keep-alive',
+      */
+      'Authorization': auth,
+      }
+    })
+    .then(data => {
+        if (!data.ok) {
+          throw Error(data.status);
+        }
+        return data.json();
+        }).then(credential => {
+        console.log(credential);
+        currentUser = credential;
+        localStorage['currentuser'] = currentUser;
+        
+        firstLastName.innerHTML = currentUser.firstname + " " + currentUser.lastname;
+        username.innerHTML = currentUser.username;
+        email.innerHTML = currentUser.email;
+        cellphone.innerHTML = currentUser.cellphone;
+
+        }).catch(e => {
+        console.log(e);
+        });
