4d2ca693ad4c1165b588ccc37b86366f6b6b3f9e
Aziz Nurhidayat <aziznurhidayata@gmail.com>
2021-06-20 20:20:10 +0700

    checkout page more or less completed

--- checkout/index.js
+++ checkout/index.js
@@ -11,9 +11,101 @@ let productsData = {};
 let orders = localStorage['orders'].split(",");
 
 //calculate total cost
-adminCost = document.querySelector('.admin-cost');
-totalCost = document.querySelector('.total-cost');
+const adminCost = document.querySelector('.admin-cost');
+const totalCost = document.querySelector('.total-cost');
+ 
+//select courier
+const courierSelect = document.querySelector('.select');
+const couriers = document.querySelector('.couriers');
+const selected = document.querySelectorAll('.courier-item');
 
+//Watch Shipping address
+const shippingAddressInput = document.querySelector('#address');
+let shippingAddress = '';
+setInterval(() => {
+  shippingAddress = shippingAddressInput.value;
+}, 700);
+//var storing chosen courier
+let courierSelected = '';
+//drop-down choice functionality
+courierSelect.addEventListener('click', () => {
+  if(couriers.className == 'couriers hidden') {
+    couriers.className = 'couriers';
+  }
+  else {
+    couriers.className = 'couriers hidden';
+  }
+})
+selected.forEach(element => {
+  element.addEventListener('click', () => {
+//    courierSelected = element.innerHTML; 
+    couriers.className = 'couriers hidden';
+    courierSelect.innerHTML = element.innerHTML;
+    switch(element.innerHTML) {
+      case "SiCepat" :
+        courierSelected = "sicepat";
+        break;
+      case "JNE Regular" :
+        courierSelected = "jneregular";
+        break;
+      case "JNE OKE" :
+        courierSelected = "jneoke";
+        break;
+      case "JNE Express" :
+        courierSelected = "jneexpress";
+        break;
+      case "Wahana" :
+        courierSelected = "wahana";
+        break;
+      case "Go-Jek" :
+        courierSelected = "gojek";
+        break;
+      case "Grab" :
+        courierSelected = "grab";
+    }
+  })
+});
+
+//select payment method
+const selectedPayment = document.querySelector('.choice');
+const paymentListCont = document.querySelector('.payments');
+const choices = document.querySelectorAll('.payment-item');
+//var storing chosen payment method
+let paymentSelected = '';
+//drop-down choice functionality
+selectedPayment.addEventListener('click', () => {
+  if(paymentListCont.className == 'payments hidden') {
+    paymentListCont.className = 'payments';
+  }
+  else {
+    paymentListCont.className = 'payments hidden';
+  }
+choices.forEach(element => {
+  element.addEventListener('click', () => {
+//    paymentSelected = element.innerHTML;
+    paymentListCont.className = 'payments hidden';
+    selectedPayment.innerHTML = element.innerHTML;
+    switch(element.innerHTML) {
+      case "Transfer BNI" :
+        paymentSelected = "trfbni";
+        break;
+      case "Transfer BRI" :
+        paymentSelected = "trfbri";
+        break;
+      case "Transfer BCA" :
+        paymentSelected = "trfbca";
+        break;
+      case "Transfer Mandiri" :
+        paymentSelected = "trfmandiri";
+        break;
+      case "Cash On Delivery (COD)" :
+        paymentSelected = "cod";
+        break;
+    }
+  })
+})
+//fetch products data from DB
+})
 fetch('http://localhost:1337/products', {
     method: 'GET',
     headers: {
@@ -61,7 +153,141 @@ fetch('http://localhost:1337/products', {
         document.querySelector('#seed').remove();
         root.append(docFrag);
 
+        
+        }).catch(e => {
+        console.log(e);
+        });
+
+
+//create transaction record to push into DB as receipt
+//get user info
+let currentUser = {};
+let auth = 'Bearer ' + localStorage['jwt'];
+fetch('http://localhost:1337/users/me', {
+    method: 'GET',
+    headers: {
+      /*
+      'Content-Type': 'application/json',
+      'Connection' : 'keep-alive',
+      */
+      'Authorization': auth,
+      }
+    })
+    .then(data => {
+        if (!data.ok) {
+          throw Error(data.status);
+        }
+        return data.json();
+        }).then(credential => {
+//        console.log(credential);
+        currentUser = credential;
+        localStorage['currentuser'] = currentUser;
 
         }).catch(e => {
         console.log(e);
         });
+//set trigger as pay button. (bayar)
+const payBtn = document.querySelector('.pay');
+let transaction = {};
+let orderArr = [];
+let j = 0;
+const order = {
+  'note' : '',
+  'itemcount' : '',
+  'product' : '',
+  'transaction' : ''
+}
+payBtn.addEventListener('click', () => {
+  //create transaction object
+  transaction = {
+    'totalprice' : totalCost.innerHTML.split('Rp. ')[1],
+    'shippingaddress' : shippingAddress,
+    'paymentmethod' : paymentSelected,
+    'courier' : courierSelected,
+    'users_permissions_user' : currentUser.id
+  }
+  //failure taming fetch, trying to use some other more forceful method
+  fetch('http://localhost:1337/transactions', {
+    method: 'POST',
+    body: JSON.stringify(transaction),
+    headers: {
+      'Content-Type': 'application/json',
+      'Connection' : 'keep-alive',
+      'Authorization': auth,
+      }
+    })
+    .then(data => {
+        if (!data.ok) {
+          throw Error(data.status);
+        }
+        return data.json();
+        }).then(credential => {
+        console.log(credential);
+        transactionID = credential.id;
+        //create order objects
+        for(index = 0; index < productsData.length; index++) {
+          //something wrong here. order spit out the same thing
+//          console.log(order)
+          if(localStorage['orders'].split(',')[index] != 0) {
+            order.note = "";
+            order.itemcount = localStorage['orders'].split(',')[index];
+            order.product = productsData[index].id;
+            order.transaction = transactionID;
+            fetch('http://localhost:1337/orderdetails', {
+              method: 'POST',
+              body: JSON.stringify(order),
+              headers: {
+                'Content-Type': 'application/json',
+                'Connection' : 'keep-alive',
+                'Authorization': auth,
+              }
+            })
+            .then(data => {
+                if (!data.ok) {
+                  throw Error(data.status);
+                }
+                return data.json();
+                }).then(credential => {
+                console.log(credential);
+                }).catch(e => {
+                console.log(e);
+                });
+          }
+        }
+        function sleep(ms) {
+          return new Promise(resolve => setTimeout(resolve, ms));
+        }
+
+        console.log("Hello");
+        sleep(1500).then(() => { window.location.href = "../payment"; });
+
+//        console.log(orderArr);
+/*
+        orderArr.forEach(element => {
+          fetch('http://localhost:1337/orderdetails', {
+          method: 'POST',
+          body: JSON.stringify(element),
+          headers: {
+            'Content-Type': 'application/json',
+            'Connection' : 'keep-alive',
+            'Authorization': auth,
+            }
+          })
+          .then(data => {
+              if (!data.ok) {
+                throw Error(data.status);
+              }
+              return data.json();
+              }).then(credential => {
+              console.log(credential);
+              }).catch(e => {
+              console.log(e);
+              });
+        }) */
+        
+        }).catch(e => {
+        console.log(e);
+        });
+})
+  
+  
